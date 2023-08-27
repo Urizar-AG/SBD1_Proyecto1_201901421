b459b5f77e24367d8e703114d0c8df4973084d03
Angel <3046108010115@ingenieria.usac.edu.gt>
2023-08-27 01:29:33 -0600

    feat: Agrega la conexión a la base de datos

--- app/src/controllers/index.controller.js
+++ app/src/controllers/index.controller.js
@@ -1,6 +1,13 @@
+import {pool}  from '../config/database.js'
+
 export const index = (req, res) => {
     res.json({
         title: 'Proyecto 1',
         message: 'Sistema para el manejo de los resultados electorales'
     })
+}
+
+export const test = async (req, res) => {
+    const [result] = await pool.query('SELECT "elecciones_generales" AS resultado')
+    res.json(result[0])
 }
--- app/src/routes/index.routes.js
+++ app/src/routes/index.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { index } from "../controllers/index.controller.js"
+import { index, test } from "../controllers/index.controller.js"
 
 const router = Router()
 
 router.get('/', index)
+router.get('/test', test)
 
 export default router

9b2fae08925a361e162adfc9921ff6fe7d71fc71
Angel <3046108010115@ingenieria.usac.edu.gt>
2023-09-06 14:11:42 -0600

    feat: Implementa la consulta 2

--- app/src/controllers/consultas.controller.js
+++ app/src/controllers/consultas.controller.js
@@ -7,7 +7,20 @@ export const consulta1 = async (req, res) => {
             (SELECT nombre_completo FROM CANDIDATO WHERE id_cargo = 1 AND PARTIDO.id_partido = id_partido) AS Presidente,
             (SELECT nombre_completo FROM CANDIDATO WHERE id_cargo = 2 AND PARTIDO.id_partido = id_partido) AS Vicepresidente
         FROM PARTIDO 
-        WHERE id_partido > -1;`
+        WHERE id_partido > -1;
+        `
     )
     res.status(200).json({Mensaje: 'Consulta 1 realizada con éxito', Registros: result})
+}
+
+export const consulta2 = async(req, res) => {
+    const [result] = await pool.query(
+        `SELECT 
+            nombre AS Partido,
+            (SELECT COUNT(id_cargo) FROM CANDIDATO WHERE (id_cargo BETWEEN 3 AND 5) AND PARTIDO.id_partido = id_partido) AS Diputados
+        FROM PARTIDO 
+        WHERE id_partido > -1;
+        `
+    )
+    res.status(200).json({Mensaje: 'Consulta 2 realizada con éxito', Registros: result})
 }
--- app/src/routes/consultas.routes.js
+++ app/src/routes/consultas.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { consulta1 } from "../controllers/consultas.controller.js"
+import { consulta1, consulta2 } from "../controllers/consultas.controller.js"
 
 const router = Router()
 
 router.get('/consulta1', consulta1)
+router.get('/consulta2', consulta2)
 
 export default router

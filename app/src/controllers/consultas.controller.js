36c14b1ef4b39bee0dd8fd6b612a6c638a19c940
Angel <3046108010115@ingenieria.usac.edu.gt>
2023-09-06 14:02:50 -0600

    feat: Implementa la consulta 1

--- app/src/controllers/consultas.controller.js
+++ app/src/controllers/consultas.controller.js
@@ -3,10 +3,11 @@ import {pool}  from '../config/database.js'
 export const consulta1 = async (req, res) => {
     const [result] = await pool.query(
         `SELECT 
-            nombre AS "Partido",
-            (SELECT nombre_completo FROM CANDIDATO WHERE id_cargo = 1 AND PARTIDO.id_partido = id_partido) AS "Presidente",
-            (SELECT nombre_completo FROM CANDIDATO WHERE id_cargo = 2 AND PARTIDO.id_partido = id_partido) AS "Vicepresidente"
-        FROM PARTIDO WHERE id_partido > -1;`
+            nombre AS Partido,
+            (SELECT nombre_completo FROM CANDIDATO WHERE id_cargo = 1 AND PARTIDO.id_partido = id_partido) AS Presidente,
+            (SELECT nombre_completo FROM CANDIDATO WHERE id_cargo = 2 AND PARTIDO.id_partido = id_partido) AS Vicepresidente
+        FROM PARTIDO 
+        WHERE id_partido > -1;`
     )
-    res.status(200).json({Mensaje: 'Consulta 1 realiaza con éxito', Registros: result})
+    res.status(200).json({Mensaje: 'Consulta 1 realizada con éxito', Registros: result})
 }

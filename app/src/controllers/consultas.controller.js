2722da012179115c8b96158e2a81840835a28496
Angel <3046108010115@ingenieria.usac.edu.gt>
2023-09-06 14:24:58 -0600

    feat: Implementa la consulta 3

--- app/src/controllers/consultas.controller.js
+++ app/src/controllers/consultas.controller.js
@@ -24,3 +24,15 @@ export const consulta2 = async(req, res) => {
     )
     res.status(200).json({Mensaje: 'Consulta 2 realizada con éxito', Registros: result})
 }
+
+export const consulta3 = async(req, res) => {
+    const [result] = await pool.query(
+        `SELECT 
+            nombre AS Partido,
+            (SELECT nombre_completo FROM CANDIDATO WHERE id_cargo = 6 AND PARTIDO.id_partido = id_partido) AS Alcalde
+        FROM PARTIDO
+        WHERE id_partido > -1;
+        `
+    )
+    res.status(200).json({Mensaje: 'Consulta 3 realizada con éxito', Registros: result})
+}
--- app/src/routes/consultas.routes.js
+++ app/src/routes/consultas.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { consulta1, consulta2 } from "../controllers/consultas.controller.js"
+import { consulta1, consulta2, consulta3 } from "../controllers/consultas.controller.js"
 
 const router = Router()
 
 router.get('/consulta1', consulta1)
 router.get('/consulta2', consulta2)
+router.get('/consulta3', consulta3)
 
 export default router
